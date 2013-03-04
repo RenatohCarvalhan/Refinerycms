ce30f10b4a5986b9a1d33e7dfd0a609735111ce3
RenatoCarvalhan <rcarvalhan@gmail.com>
2013-03-04 14:34:18 -0300

    cms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PianoPlus::Application.config.secret_token = 'fcd5a5def5a4479da4618210475eb77e11e94e1fb5536797ff3e2f0b031837ce95050d561455d2749cec8a3fd235222fff6a7b345dddb8148903acb52676b033'
